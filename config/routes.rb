c8f4f590090a988743ce8560b444c0f2f9d76b95
Michael Epelboim <sdrmike@outlook.com>
2022-11-17 18:10:27 +0800

    pets app complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'pets#index'
+
+  get 'pets', to: 'pets#index'
+  get 'pets/new', to: 'pets#new'
+  get 'pets/:id', to: 'pets#show', as: :pet
+  post 'pets', to: 'pets#create'
+  get 'pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch 'pets/:id', to: 'pets#update'
+  delete 'pets/:id', to: 'pets#destroy'
+
+  # resources :pets
 end
